a3400338968a0596d5cf6fc5b9c1d0a6df3d9997
vincez14 <vincez14@vt.edu>
2018-02-20 17:56:32 -0500

    Final copy of In-Class Assignment.

--- L11/mpi.c
+++ L11/mpi.c
@@ -4,16 +4,16 @@
 
 int main(int argc, char **argv) {
 
-  double rank;
+  int rank;
   int size;
 
-  // A. Initialize & Finalize the MPI session
+  // A. Initialize the MPI session
   MPI_Init(&argc, &argv);
   
   // B. Find the rank
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  // C.
+  // C. Find the number(size) of MPI processes
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   //need running tallies
@@ -21,11 +21,11 @@ int main(int argc, char **argv) {
   long long int Ncircle;
 
   //seed random number generator
-  double seed = rank;
-  srand48(seed);
+  double seed = (double) rank; //cast rank to be a double
+  srand48(seed); //set the random number seed to be the MPI process rank
 
   for (long long int n=0; n<1000000000;n++) {
-    //gererate two random numbers
+    //generate two random numbers
     double rand1 = drand48(); //drand48 returns a number between 0 and 1
     double rand2 = drand48();
     
@@ -37,11 +37,30 @@ int main(int argc, char **argv) {
     Ntotal++;
   }
 
-  double pi = 4.0*Ncircle/ (double) Ntotal;
+  // local calculation of pi
+  double pi = 4.0*Ncircle/ (double) Ntotal; 
+  double sum; // variable for the sum to be stored in
 
-  printf("Our estimate of pi is %f \n", pi);
+  // E. Perform an All Reduce function to sum the local estimates
+  MPI_Allreduce(&pi,       // send buffer
+                &sum,       // receive buffer
+                1,          // count (num of entries)
+                MPI_DOUBLE, // data type
+                MPI_SUM,    // MPI operation
+                MPI_COMM_WORLD);
+
+  // F. Only Rank zero prints out the estimate for pi every 100 steps
+  if(rank == 0 && Ntotal%100 == 0) {  //only when rank=0 and only every 100 steps
+    // our calculated value of pi
+    pi = ((double) sum/(double) Ntotal)*4.0; //divides by the number of processes
+
+    // print the calculated value of pi
+    printf("Our estimate of pi is %f \n", pi);   
+  }
 
   // A. Initialize and finalize the MPI session
   MPI_Finalize();
-  return 0;
+
+  
+  return 0; 
 }
