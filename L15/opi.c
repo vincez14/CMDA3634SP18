e919af02d215f251d259c58a7cd3eb45c3cfaf10
vincez14 <vincez14@vt.edu>
2018-03-13 16:59:25 -0400

    Completed In-Class Assignment Code for opi.c

--- L15/opi.c
+++ L15/opi.c
@@ -2,35 +2,51 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include <omp.h>
+
 int main(int argc, char **argv) {
 
   //seed random number generator
-  // Q2b: get the number of threads to run with from agrv and 
+  // Q2b: get the number of threads to run with from argv and 
   // add OpenMP API code to set number of threads here
-  int Nthreads = 1;
+  int Nthreads = atoi(argv[1]);
   
+  // OpenMP call to set number of threads
+  omp_set_num_threads(Nthreads);
+
   struct drand48_data *drandData; 
   drandData = (struct drand48_data*) malloc(Nthreads*sizeof(struct drand48_data));
 
   // Q2c: add an OpenMP parallel region here, wherein each thread initializes 
   //      one entry in drandData using srand48_r and seed based on thread number
-  long int seed = 0;
-  srand48_r(seed, drandData+0);
+
+  #pragma omp parallel 
+  {
+    int rank = omp_get_thread_num();
+
+    long int seed = rank;
+    srand48_r(seed, drandData+rank);
+  }
 
   long long int Ntrials = 10000000;
 
-
   //need running tallies
   long long int Ntotal=0;
-  long long int Ncircle=0;
+  long long int Ncircle=0; 
 
+  // start time
+  double startTime = omp_get_wtime();
+
+  #pragma omp parallel for reduction(+:Ncircle)
   for (long long int n=0; n<Ntrials; n++) {
     double rand1;
     double rand2;
 
-    //gererate two random numbers (use the thread id to offset drandData)
-    drand48_r(drandData+0, &rand1);
-    drand48_r(drandData+0, &rand2);
+    int rank = omp_get_thread_num();
+
+    //generate two random numbers (use the thread id to offset drandData)
+    drand48_r(drandData+rank, &rand1);
+    drand48_r(drandData+rank, &rand2);
     
     double x = -1 + 2*rand1; //shift to [-1,1]
     double y = -1 + 2*rand2;
@@ -42,12 +58,21 @@ int main(int argc, char **argv) {
     if (n%100 ==0) {
       double pi = 4.0*Ncircle/ (double) (n);
       printf("Our estimate of pi is %g \n", pi);
-    }
-  }
+    } // end if
+  } // end for loop
 
+  // back to serial
   double pi = 4.0*Ncircle/ (double) (Ntotal);
   printf("Our final estimate of pi is %g \n", pi);
 
+  // end time
+  double endTime = omp_get_wtime();
+  
+  // print total run time
+  double runtime = endTime - startTime;
+  printf("Runtime = %g \n", runtime);
+
+  // free the data
   free(drandData);
   
   return 0;
